b737f554d0cee655f716c9d650af76081d7e843a
mateusz.lakota <MateuszLakota@outlook.com>
2022-05-24 00:05:54 +0200

    Implement fix to controller

--- src/main/kotlin/com/lakota/api/controller/ConcretePriceCalculatorController.kt
+++ src/main/kotlin/com/lakota/api/controller/ConcretePriceCalculatorController.kt
@@ -5,16 +5,17 @@ import com.lakota.data.dto.ConcretePriceDTO
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
+import java.math.BigDecimal
 
 @RestController
-class ConcreteConcretePriceController(private val client: BuildingMaterialsClient) : ConcretePriceApi {
+class ConcretePriceCalculatorController(private val client: BuildingMaterialsClient) : ConcretePriceCalculatorApi {
+
     override fun getPrice(): ResponseEntity<ConcretePriceDTO> {
         val cementPrice = client.retrieveCementPrice()
         val chippingsPrice = client.retrieveChippingsPrice()
         val sandPrice = client.retrieveSandPrice()
         val waterPrice = client.retrieveWaterPrice()
-        val sum = Integer.valueOf(cementPrice) + Integer.valueOf(waterPrice) + Integer.valueOf(sandPrice)
-        + Integer.valueOf(chippingsPrice)
+        val sum = BigDecimal(cementPrice) + BigDecimal(waterPrice) + BigDecimal(sandPrice) + BigDecimal(chippingsPrice)
         return responseWith<ConcretePriceDTO>(HttpStatus.OK).body(ConcretePriceDTO(sum))
     }
 
